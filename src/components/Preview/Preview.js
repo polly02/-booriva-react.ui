1e98b54b43d7f918a94e3c9f17f87376a7a5a263
polina <polina.zdanovich.02@mail.ru>
2023-02-04 01:09:55 +0300

    booriva react main page

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -1,14 +1,43 @@
 import style from "./Preview.module.css"
 
-function Preview(){
-return(
-    <div className={style["wrapper"]}>
-        <div className={style["img-womens"]}></div>
-        <div className={style["img-coll"]}></div>
-        <div className={style["img-store"]}></div>
-        <div></div>
-    </div>
-)
+function Preview() {
+    return (
+        <div className={style["wrapper"]}>
+            <div className={style["img-womens"]}></div>
+            <div className={style["price"]}>
+                <div className={style["price-1099"]}></div>
+                <div className={style["price-1299"]}></div>
+                <div className={style["price-2499"]}></div>
+                <div className={style["price-899"]}></div>
+            </div>
+            <div className={style["name"]}>
+                <p>Cвитшот вставка клетка</p>
+                <p>Платье прозрачное в цветочек черное</p>
+                <p>Бомбер Gone Crazy хаки</p>
+                <p>Платье-футболка рыбы в аквариуме</p>
+            </div>
+            <div className={style["img-coll"]}></div>
+            <div className={style["img-store"]}></div>
+
+            <div className={style["advantages"]}>
+                <div>
+                    <div className={style["icon-send"]}></div>
+                    <h2>Отправка день в день</h2>
+                    <p>При заказе до 1800</p>
+                </div>
+                <div>
+                    <div className={style["icon-change"]}></div>
+                    <h2>Легкий возврат/обмен</h2>
+                    <p>В течении 14 дней</p>
+                </div>
+                <div>
+                    <div className={style["icon-made"]}></div>
+                    <h2>Сделано в Украине</h2>
+                    <p>Прям в Киеве</p>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Preview
--- src/pages/PreviewPage/PreviewPage.js
+++ src/pages/PreviewPage/PreviewPage.js
@@ -1,13 +1,16 @@
 import Footer from "../../components/Footer/Footer"
 import Header from "../../components/Header/Header"
 import Preview from "../../components/Preview/Preview"
+import style from "./PreviewPage.module.css"
 
-function  PreviewPage(){
-    return(
+function PreviewPage() {
+    return (
         <>
-        <Header></Header>
-        <Preview></Preview>
-        <Footer></Footer>
+            <Header></Header>
+            <div className={style["preview-component"]}>
+                <Preview></Preview>
+            </div>
+            <Footer></Footer>
         </>
     )
 }
