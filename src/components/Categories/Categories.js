12b1ebf2bc19c95d5c27258b2e85b5b936de5fc0
polina <polina.zdanovich.02@mail.ru>
2023-02-06 00:14:11 +0300

    category page

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -15,10 +15,26 @@ function Categories() {
                     <p>Платье прозрачное в цветочек чёрное</p>
                     <div className={style["price"]}>1 299 ₴</div>
                 </div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+                <div className={style["bomber"]}>
+                    <div className={style["bomber-img"]}></div>
+                    <p>Платье прозрачное в цветочек чёрное</p>
+                    <div className={style["price"]}>1 299 ₴</div>
+                </div>
+                <div className={style["dress-t-shirt"]}>
+                    <div className={style["dress-t-shirt-img"]}></div>
+                    <p>Платье прозрачное в цветочек чёрное</p>
+                    <div className={style["price"]}>1 299 ₴</div>
+                </div>
+                <div className={style["sweatshirt2"]}>
+                    <div className={style["sweatshirt-img"]}></div>
+                    <p>Платье прозрачное в цветочек чёрное</p>
+                    <div className={style["price"]}>1 299 ₴</div>
+                </div>
+                <div className={style["dress2"]}>
+                    <div className={style["dress-img"]}></div>
+                    <p>Платье прозрачное в цветочек чёрное</p>
+                    <div className={style["price"]}>1 299 ₴</div>
+                </div>
             </div>
         </div>
     )
